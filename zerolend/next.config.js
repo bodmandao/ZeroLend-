5507483ddc20588b32bcd30ca2a01f924780c4a8
bodmandao <154741685+bodmandao@users.noreply.github.com>
2026-03-14 17:07:45 +0000

    upgrade webpack to turbo

--- zerolend/next.config.js
+++ zerolend/next.config.js
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@provablehq/sdk'],
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-    };
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
